10917327b560f64dd0909184bd7c248743dcc48f
Om Rawat <omrawat23@gmail.com>
2024-10-29 22:25:01 +0530

    v10

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import { User,skills } from '@/components/Userinfo'
 
 export default function Banner() {
   return (
-    <div className="max-w-4xl xs:px-6 lg:px-8 md:m-4 xs:m-4">
+    <div className="max-w-4xl xs:px-6 lg:px-8 md:m-4 xs:m-4 mt-6">
       <div className="border border-black rounded-lg overflow-hidden">
         <div className="bg-[#fffaf4] p-4 sm:p-6 relative">
           {/* Browser-like controls */}
